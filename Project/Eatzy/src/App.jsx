d32c1e562e88f91a2c696d74af9405aef6b4a331
rohit-roshan123 <varrerohitroshan@gmail.com>
2025-07-15 11:44:56 +0530

    imported flatty icons and creating navigation bar

--- Project/Eatzy/src/App.jsx
+++ Project/Eatzy/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Head from './Components/Head'
 
 function App() {
   
 
   return (
     <>
-    Hello
+    <Head />
     </>
   )
 }
---
+++ Project/Eatzy/src/Components/Head.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div className='w-full shadow-md h-24 flex justify-center items-center'>
+        <div className='border border-black w-[70%]'>
+            <div className="flex items-center">
+            <img className="w-24" src='https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png' />
+            <p className='font-bold border-b-2 pb-1 border-black'>Other</p>
+            <i class="fi fi-rs-angle-small-down"></i>
+
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Head
