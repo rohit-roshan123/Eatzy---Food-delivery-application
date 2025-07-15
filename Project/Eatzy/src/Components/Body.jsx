8a9d462a5c295101c8d2d145acb30392dd1b82b3
rohit-roshan123 <varrerohitroshan@gmail.com>
2025-07-15 20:08:56 +0530

    creating menu

--- Project/Eatzy/src/Components/Body.jsx
+++ Project/Eatzy/src/Components/Body.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Body() {
+    const [data, setData] = useState([]);
+
+    async function fetchData(){
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const result = await data.json() ; 
+        console.log(result);
+    }
+
+    useEffect(()=>{
+        fetchData()
+    }, [])
+
   return (
     <div className='w-full '>
 
--- Project/Eatzy/src/Components/Head.jsx
+++ Project/Eatzy/src/Components/Head.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function Head() {
   return (
-    <div className="w-full shadow-md h-24 flex justify-center items-center">
-      <div className="border border-black w-[80%] flex justify-between">
+    <div className="w-full shadow-md h-22 flex justify-center items-center">
+      <div className="w-[80%] flex justify-between">
         <div className="flex items-center">
           <img
-            className="w-24"
+            className="w-20"
             src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png"
           />
           <div className="flex items-center gap-2">
-            <p className="font-bold border-b-2 border-black">Others</p>
+            <p className="font-bold border-b-2 border-black">Other</p>
             <i className="fi mt-2 text-orange-500 text-2xl fi-rs-angle-small-down"></i>
           </div>
         </div>
